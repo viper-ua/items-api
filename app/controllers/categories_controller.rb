5fb99857d84bd22d1f607dec31fb1390586ccadc
Vladimir Zhilyaev <vladimir@zhilyaev.kyiv.ua>
2017-12-14 00:18:21 +0200

    Small code refactoring - less items info in category view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,7 +6,7 @@ class CategoriesController < ApplicationController
 
   def show()
     category = Category.find(params[:id])
-    items = category.items
+    items = category.items.map { |item| {id: item.id, name: item.name} }
     json_response([category, items])
   end
 end
